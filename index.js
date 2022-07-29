6dee63dc803fa44be83669305a0eb7e76d964427
MohammadRhamdan <moh.rhamdanf52121023@gmail.com>
2022-07-29 19:57:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("brand-icon");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+// var btnContainer = document.querySelector(".navigation-manual");
+// var btns = btnContainer.querySelector(".manual-btn");
+
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     if (current.length > 0) {
+//       current[0].className = current[0].className.replace(" active", "");
+//     }
+//     this.className += " active";
+//   });
+// }
